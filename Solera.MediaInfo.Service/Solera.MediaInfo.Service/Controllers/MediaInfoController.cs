ac09d7c8967c0e31e1fb77610c3351e6e8f3556c
Kondavagilu <Ram.Kondavagilu@solera.com>
2019-01-26 00:25:54 -0600

    Changes for photo upload

--- Solera.MediaInfo.Service/Solera.MediaInfo.Service/Controllers/MediaInfoController.cs
+++ Solera.MediaInfo.Service/Solera.MediaInfo.Service/Controllers/MediaInfoController.cs
@@ -21,11 +21,16 @@ namespace Solera.MediaInfo.Service.Controllers
     {
         // POST: api/MediaInfo
         [HttpPost("photo")]
-        public async Task<object> PostPhoto([FromForm] PhotoUploadRequest request)
+        public async Task<object> PostPhoto([FromBody] PhotoUploadRequest request)
         {
+            Console.WriteLine($"Request is {request}");
+            Console.WriteLine($"Request fileName is {request.FileName}");
+            Console.WriteLine($"Request ValuationNumber is {request.ValuationNumber}");
+            Console.WriteLine($"Request photo is {request.photo}");
+
             BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAID3MAMFSUEUCVWFQ", "4THQSOA10tKIy3yxjWYz1jLOodcsgcVUXZVrycgS");
             IAmazonS3 _s3Client = new AmazonS3Client(credentials, RegionEndpoint.USEast1);
-
+            Console.WriteLine($"Client is created {_s3Client}");
             //This code needs to be moved to the actual externalApi section and through Interfaces and dependency Injection.
             // Just that I am trying the possibility of moving this code to Content delivery service instead of creating another service
             // If that plan falls apart and if we need to maintain this service, this code needs to be refactored accordingly. Whenever that
@@ -35,19 +40,23 @@ namespace Solera.MediaInfo.Service.Controllers
             {
                 using (var memStream = new MemoryStream())
                 {
-                    request.photo.CopyTo(memStream);
-
-                    Console.WriteLine("POST PHOTO : Photo data copied to MemoryStream object!");
-                    var fileTransferUtility = new TransferUtility(_s3Client);
-
-                    await fileTransferUtility.UploadAsync(memStream,
-                                               "gotimedrivervideos/" + request.ValuationNumber, request.FileName);
-                    return StatusCode(StatusCodes.Status200OK, "Photo uploaded successful!!");
+                    Console.WriteLine($"Photo data is being copied to memorystream, size {request.photo.Length}");
+                    Console.WriteLine($"The UploadAsync is invoked with memsetream length {request.photo.Length}, valuation number {request.ValuationNumber} and filename {request.FileName}");
+                    return null;
+                    //request.photo.CopyTo(memStream);
+                    //Console.WriteLine("POST PHOTO : Photo data copied to MemoryStream object!"); 
+                    //var fileTransferUtility = new TransferUtility(_s3Client);
+                    //Console.WriteLine($"The UploadAsync is invoked with memsetream length {memStream.Length}, valuation number {request.ValuationNumber} and filename {request.FileName}");
+                    //await fileTransferUtility.UploadAsync(memStream,
+                    //                           "gotimedrivervideos/" + request.ValuationNumber, request.FileName);
+                    //Console.WriteLine("File Uploaded successfully");
+                    //return StatusCode(StatusCodes.Status200OK, "Photo uploaded successful!!");
                 }
             }
             catch (Exception e)
             {
-                return StatusCode(StatusCodes.Status200OK, $"Photo uploaded failed with error {e.Message}!!");
+                Console.WriteLine($"The photo upload failed with the error {e.Message}");
+                return StatusCode(StatusCodes.Status400BadRequest, $"Photo uploaded failed with error {e.Message}!!");
             }
         }
     }
