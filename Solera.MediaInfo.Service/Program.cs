dfbcd9e1d27f4ec24bc92afc4543cae645b922d4
Victor Escajeda <victor.escajeda@audatex.com>
2020-01-07 16:21:57 -0600

    Implement serilog for logging incomming request details [GTA-2197]

--- Solera.MediaInfo.Service/Program.cs
+++ Solera.MediaInfo.Service/Program.cs
@@ -1,32 +1,64 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Solera.MediaInfo.Service.Constants;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using Steeltoe.Extensions.Logging;
+using System;
+using System.IO;
 
 namespace Solera.MediaInfo.Service
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable(EnvironmentVariable.ASPNETCORE_ENVIRONMENT) ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            // Add VCAP_* configuration data
+            .AddCloudFoundry()
+            .Build();
+
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new Serilog.LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Media Info Service");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return -1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-
-                .UseDefaultServiceProvider(options => options.ValidateScopes = false)
-                .UseCloudFoundryHosting()
-                .ConfigureAppConfiguration((builderContext, config) =>
-                {
-                    config.AddCloudFoundry();
-                })
-                .ConfigureLogging((builderContext, loggingBuilder) =>
-                {
-                    loggingBuilder.ClearProviders();  // avoid duplicated log entries
-                    loggingBuilder.AddDynamicConsole();
-                });
+                    .UseSerilog()
+                    .UseKestrel(k => k.AddServerHeader = false)
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseDefaultServiceProvider(options => options.ValidateScopes = false)
+                    .UseCloudFoundryHosting()
+                    // Add VCAP_* configuration data
+                    .AddCloudFoundry()
+                    .ConfigureLogging((builderContext, loggingBuilder) =>
+                    {
+                        loggingBuilder.ClearProviders();  // avoid duplicated log entries
+                        loggingBuilder.AddDynamicConsole();
+                    })
+                    .UseStartup<Startup>();
     }
 }
